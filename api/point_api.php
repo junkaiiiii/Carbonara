7c10eceba4c3c2af137070f0be41b2525b616c8a
junkaiiiii <shojunkai2016@gmail.com>
2026-01-16 12:18:23 +0800

    fix users available points
    sum(points log) - sum(redeemed Points)
    
    fade in animation to driver pop up

--- api/point_api.php
+++ api/point_api.php
@@ -11,19 +11,21 @@ include "../db_connect.php";
 $method = $_SERVER["REQUEST_METHOD"];
 
 if ($method === "GET"){
-    $sql = "SELECT COALESCE(SUM(points_earned),0) AS points 
-            FROM points_log
-            WHERE user_id = ? 
-            GROUP BY user_id";
+    $sql = "SELECT 
+    (
+        COALESCE((SELECT SUM(points_earned) FROM points_log WHERE user_id = ?), 0) 
+        - 
+        COALESCE((SELECT SUM(cost) FROM redemption WHERE user_id = ?), 0)
+    ) AS available_points;";
     $stmt = mysqli_prepare($conn,$sql);
-    mysqli_stmt_bind_param($stmt, 's', $_SESSION['user_id']);
+    mysqli_stmt_bind_param($stmt, 'ss', $_SESSION['user_id'], $_SESSION['user_id']);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
 
     if ($result){
         $row = mysqli_fetch_assoc($result);
 
-        respond(intval($row['points']));
+        respond(intval($row['available_points']));
     }
 }else{
     respond(['error'=>'invalid request method'],400);
