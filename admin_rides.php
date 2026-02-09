a30063146d77e777e1296a28c9d2638070be7e09
Jet1211 <thumjethro@gmail.com>
2026-02-09 10:19:49 +0800

    Update admin_rides.php

--- admin_rides.php
+++ admin_rides.php
@@ -11,7 +11,6 @@
     <link rel="stylesheet" href="styles/admin_style.css">
     <link rel="stylesheet" href="styles/admin_rides.css">
     <link rel="stylesheet" href="styles/admin_header.css"> 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     <?php
